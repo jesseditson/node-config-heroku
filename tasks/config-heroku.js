6363594026b1a588c1f463db2549aaf81f15bf1d
Jesse Ditson <jesse.ditson@gmail.com>
2013-03-09 23:52:03 -0800

    Add app option to all heroku commands, convert to multi taks

--- tasks/config-heroku.js
+++ tasks/config-heroku.js
@@ -6,24 +6,11 @@ var save = require('../lib/commands/save')
 module.exports = function(grunt){
   if(!grunt) return false
   
-  function getConfig () {
-    var config = grunt.config('config-heroku') || {};
-
-    // Look for and process grunt template stings
-    var keys = ['configname', 'varname'];
-    keys.forEach(function(key) {
-      if (config.hasOwnProperty(key)) {
-        config[key] = grunt.template.process(config[key]);
-      }
-    });
-    return config
-  }
-  
-  grunt.registerTask('config-heroku', 'Save local config to heroku.', function() {
+  grunt.registerMultiTask('config-heroku', 'Save local config to heroku.', function() {
     var done = this.async()
-    var config = getConfig()
+    var config = this.data
     grunt.log.writeln('Saving local config to Heroku...'.blue)
-    save.call(grunt,config, function(conf,cb){ return cb(true) }, function(success){
+    save.call(grunt, config, function(conf,cb){ return cb(true) }, function(success){
       grunt.log.writeln("Completed saving config.".blue)
       done(success)
     })
